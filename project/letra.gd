9b20f7b4f161f0e11dfa983a36477b026f763e31
miguel freitas <migueljulio.freitas@gmail.com>
2021-02-25 00:12:14 -0300

    bug corrigido

--- project/letra.gd
+++ project/letra.gd
@@ -8,11 +8,36 @@ onready var spot2 = root_node.get_node("spotLetra2")
 onready var spot3 = root_node.get_node("spotLetra3")
 onready var spot4 = root_node.get_node("spotLetra4")
 onready var spot5 = root_node.get_node("spotLetra5")
+
+onready var letra = root_node.get_node("letra")
+onready var letra2 = root_node.get_node("letra2")
+onready var letra3 = root_node.get_node("letra3")
+onready var letra4 = root_node.get_node("letra4")
+onready var letra5 = root_node.get_node("letra5")
+onready var letra6 = root_node.get_node("letra6")
+onready var letra7 = root_node.get_node("letra7")
+onready var letra8 = root_node.get_node("letra8")
+onready var letra9 = root_node.get_node("letra9")
+
 onready var initialPosition = get_position()
 
 func _ready():
 	pass # Replace with function body.
 
+func podeOcupar(posicaoDoSpot):
+	if !(posicaoDoSpot == letra.global_position || posicaoDoSpot == letra2.global_position || posicaoDoSpot == letra3.global_position || posicaoDoSpot == letra4.global_position || posicaoDoSpot == letra5.global_position || posicaoDoSpot == letra6.global_position):
+		if(letra7):
+			if !(posicaoDoSpot == letra7.global_position):
+				if(letra8 && letra9):
+					return !(posicaoDoSpot == letra8.global_position || posicaoDoSpot == letra9.global_position)
+				else:
+					return true
+			else:
+				return false
+		else:
+			return true
+	else:
+		return false
 
 func _process(delta):
 	if dragMouse:
@@ -41,35 +66,35 @@ func _on_Area2D_input_event(viewport, event, shape_idx):
 
 
 func _on_spotLetra_area_entered(area):
-	if dragMouse:
+	if (dragMouse && podeOcupar(spot.global_position)):
 		dragMouse = false
 		set_position(spot.global_position)
 	pass
 
 
 func _on_spotLetra2_area_entered(area):
-	if dragMouse:
+	if (dragMouse && podeOcupar(spot2.global_position)):
 		dragMouse = false
 		set_position(spot2.global_position)
 	pass
 
 
 func _on_spotLetra3_area_entered(area):
-	if dragMouse:
+	if (dragMouse && podeOcupar(spot3.global_position)):
 		dragMouse = false
 		set_position(spot3.global_position)
 	pass
 
 
 func _on_spotLetra4_area_entered(area):
-	if dragMouse:
+	if (dragMouse && podeOcupar(spot4.global_position)):
 		dragMouse = false
 		set_position(spot4.global_position)
 	pass
 
 
 func _on_spotLetra5_area_entered(area):
-	if dragMouse:
+	if (dragMouse && podeOcupar(spot5.global_position)):
 		dragMouse = false
 		set_position(spot5.global_position)
 	pass
